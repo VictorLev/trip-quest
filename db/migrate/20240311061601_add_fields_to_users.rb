07fe35b2dd21fc9773e08c896b7b86c598f3f965
Jacopo Riani jacopo.riani@gmail.com <jacopo.riani@gmail.com>
2024-03-11 17:24:19 +1100

    seed

---
+++ db/migrate/20240311061601_add_fields_to_users.rb
@@ -0,0 +1,9 @@
+class AddFieldsToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :age, :integer
+    add_column :users, :location, :string
+    add_column :users, :gender, :string
+  end
+end
