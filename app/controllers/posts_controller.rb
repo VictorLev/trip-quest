cb9382a76a5c9e030a9c12084fdf1f2c1376a4c1
Jacopo Riani jacopo.riani@gmail.com <jacopo.riani@gmail.com>
2024-03-12 12:40:46 +1100

    added live posts with action cable

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,23 @@
 class PostsController < ApplicationController
+  def create
+    @feed = Feed.find(params[:feed_id])
+    @post = Post.new(post_params)
+    @post.feed = @feed
+    @post.user = current_user
+    if @post.save
+      FeedChannel.broadcast_to(
+        @feed,
+        render_to_string(partial: "post", locals: {post: @post})
+      )
+      head :ok
+    else
+      render "feeds/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
---
+++ app/javascript/controllers/feed_subscription_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus"
+import { createConsumer } from "@rails/actioncable"
+
+// Connects to data-controller="feed-subscription"
+export default class extends Controller {
+  static values = { feedId: Number }
+  static targets = ["posts", "inputs"]
+  connect() {
+    this.channel = createConsumer().subscriptions.create(
+      { channel: "FeedChannel", id: this.feedIdValue },
+      { received: data => this.#insertPostAndScrollDown(data)}
+    )
+  }
+  #insertPostAndScrollDown(data) {
+    this.postsTarget.insertAdjacentHTML("beforeend", data)
+    window.scrollTo(0, document.body.scrollHeight);
+    // posts = document.querySelector(".posts")
+    // posts.scrollTo(0, posts.scrollHeight);
+  }
+
+  resetForm(event) {
+    event.target.reset()
+  }
+
+  disconnect() {
+    console.log("Unsubscribed from the feed")
+    this.channel.unsubscribe()
+  }
+}
