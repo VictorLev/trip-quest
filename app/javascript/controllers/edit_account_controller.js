f34ed22f3b2670d7a81c5f2abcfcc4e68fa4e6c0
Victor Levesque <levesque.vic@gmail.com>
2024-03-12 17:35:51 +1100

    master

--- app/javascript/controllers/edit_account_controller.js
+++ app/javascript/controllers/edit_account_controller.js
@@ -5,31 +5,12 @@ export default class extends Controller {
   connect() {
   }
 
-  static targets = ["infos", "form", "card"]
+  static targets = ["infos", "form"]
 
-  get userId() {
-    return this.formTarget.dataset.userId;
-  }
 
   displayForm() {
     this.infosTarget.classList.add("d-none")
     this.formTarget.classList.remove("d-none")
-  }
-
-  update(event) {
-    event.preventDefault()
-    const url = '/account/'
-    console.log(url)
-    fetch(url, {
-      method: "PATCH",
-      headers: { "Accept": "text/plain" },
-      body: new FormData(this.formTarget),
-      'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content
-    })
-      .then(response => response.text())
-      .then((data) => {
-        this.cardTarget.outerHTML = data
-      })
   }
 
 }
