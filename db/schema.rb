b5d19329837c4ca53e55058c41d69a31dffac486
Jacopo Riani jacopo.riani@gmail.com <jacopo.riani@gmail.com>
2024-03-13 12:02:27 +1100

    attempt map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_12_235156) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_13_003545) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -126,6 +126,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_12_235156) do
     t.bigint "car_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["car_id"], name: "index_trips_on_car_id"
   end
 
