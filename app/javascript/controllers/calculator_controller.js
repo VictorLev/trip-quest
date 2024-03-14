f7459c34812069fecfedee2eb773acfa61705aa0
Victor Levesque <levesque.vic@gmail.com>
2024-03-14 11:16:13 +1100

    it works

--- app/javascript/controllers/calculator_controller.js
+++ app/javascript/controllers/calculator_controller.js
@@ -4,7 +4,7 @@ import mapboxgl from 'mapbox-gl'
 
 // Connects to data-controller="calculator"
 export default class extends Controller {
-  static targets = ["startpoint", "endpoint", "form", "button"];
+  static targets = ["startpoint", "endpoint", "form", "button", "map"];
 
   static values = {
     apiKey: String,
@@ -16,7 +16,7 @@ export default class extends Controller {
     mapboxgl.accessToken = this.apiKeyValue
 
     this.map = new mapboxgl.Map({
-      container: this.element,
+      container: this.mapTarget,
       style: "mapbox://styles/mapbox/streets-v10",
       center: [144.9631, -37.8136],
       zoom: 12
@@ -28,31 +28,38 @@ export default class extends Controller {
       this.direction,
       'top-left'
     );
+
+    this.#addMarkersToMap()
+    this.#fitMapToMarkers()
+
   }
 
   save(event) {
     event.preventDefault()
 
-    // Ensure that "startpoint" target is available
-    console.log(this.formTarget);
-
-    if (!this.hasStartpointTarget) {
-      console.error("Missing target element 'startpoint' for 'calculator' controller");
-      return;
-    }
-
-    console.log(this.direction.getOrigin().geometry.coordinates.toString());
-    console.log(this.direction.getDestination().geometry.coordinates.toString());
-
     // Update the values of the start and end targets
-    // this.startpointTarget.value = this.direction.getOrigin().geometry.coordinates.toString();
-    // this.endpointTarget.value = this.direction.getDestination().geometry.coordinates.toString();
-
-    // Log the updated values for verification
-    console.log("Start value:", this.startpointTarget.value);
-    console.log("End value:", this.endpointTarget.value);
+    this.startpointTarget.value = this.direction.getOrigin().geometry.coordinates.toString();
+    this.endpointTarget.value = this.direction.getDestination().geometry.coordinates.toString();
 
     // Submit the form
     this.formTarget.submit();
   }
+
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.sp_info_html)
+      console.log(marker)
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
+        .addTo(this.map)
+    })
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  }
+
 }
