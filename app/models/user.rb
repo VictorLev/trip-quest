3cadb3037b6036bdaeb55c76c476310c389016f1
Emma Stone <emma.stone1234@icloud.com>
2024-03-15 14:17:17 +1100

    Added filter funtion to my trips

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
 
+  has_one_attached :photo
+  
   has_many :cars, dependent: :destroy
   has_many :posts, dependent: :destroy
-  has_one_attached :photo
+
   has_many :planned_routes, dependent: :destroy
 
+  has_many :projected_rewards, through: :planned_routes
+  has_many :trips, through: :cars
 end
